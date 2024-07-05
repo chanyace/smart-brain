44ed754930ed623d970f3dd9a8f35fbd9b334c72
Mint <chanya.yuleee@gmail.com>
2024-07-05 10:56:14 +0100

    prefix clarifai

--- src/App.js
+++ src/App.js
@@ -23,9 +23,10 @@ const APP_ID = 'test';
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = 'aa7f35c01e0642fda5cf400f543e7c40';  
 
-// const app = new Clarifai.App({
-//  apiKey: 'dcd4cd90962b401cb75e200c3d8f6ec6'
-// });
+const app = new Clarifai.App({
+  apiEndpoint: 'https://api.clarifai.com',
+  apiKey: 'dcd4cd90962b401cb75e200c3d8f6ec6'
+});
 
 const returnClarifaiRequestOptions = (imageUrl) => {
 
@@ -186,32 +187,32 @@ class App extends Component {
     this.state = initialState;
   }
 
-    loadUser = (data) => {
-      this.setState({user: {
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        entries: data.entries,
-        joined: data.joined
-      }})
-    }
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
 
-    calculateFaceLocation = (data) => {
-      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-      const image = document.getElementById('inputimage');
-      const width = Number(image.width);
-      const height = Number(image.height);
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
+  }
   
-    displayFaceBox = (box) => {
-      this.setState({box: box});
-    }
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
   
   onInputChange = (event) => {
     this.setState({input: event.target.value});
@@ -219,31 +220,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
-      .then(response => response.json())
+    app.models
+      .predict(
+      "6dc7e46bc9124c5c8824be4822abe105",
+      this.state.input)
       .then(response => {
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count}))
-            })
-            .catch(console.log)
-
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        console.log(response);
+        this.calculateFaceLocation(response)
+      
       })
       .catch(err => console.log(err));
   }
